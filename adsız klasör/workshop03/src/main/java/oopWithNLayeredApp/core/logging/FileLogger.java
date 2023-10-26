f88c8559d0e244d4df7877d22e17cc841a6f54cf
EbrarKagit <148901000+EbrarKgt@users.noreply.github.com>
2023-10-26 22:31:37 +0300

    .

--- adsız klasör/workshop03/src/main/java/oopWithNLayeredApp/core/logging/FileLogger.java
+++
@@ -1,8 +0,0 @@
-package oopWithNLayeredApp.core.logging;
-
-public class FileLogger implements Logger {
-    public void log (String data){
-        System.out.println("Dosyaya loglandı : " + data);
-    }
-
-}
